f34a8a04645926ca62ae5621e3bd92acd279531d
JMH <ajedmh@gmail.com>
2021-03-09 19:39:54 +0200

    added final code

--- mod4-assign/script.js
+++ mod4-assign/script.js
@@ -33,15 +33,14 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function(){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
+for (var i=0; i<names.length; i++){
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -56,9 +55,11 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (names[i].charAt(0) == "J" || names[i].charAt(0) == "j") {
+    byeSpeaker.speak(names[i]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i]);
   }
 }
+})();
+
